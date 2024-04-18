cb577e5c13cb79d8384e1999e1b9bd04395f40d6
Looped312 <139386975+Looped312@users.noreply.github.com>
2024-04-18 18:27:20 -0400

    Update EditOverlayViewModel.kt
    add more imports i think would be useful for making the overlay editor

--- src/android/app/src/main/java/io/github/lime3ds/android/viewmodel/EditOverlayViewModel.kt
+++ src/android/app/src/main/java/io/github/lime3ds/android/viewmodel/EditOverlayViewModel.kt
@@ -3,6 +3,9 @@ package io.github.lime3ds.android.viewmodel;
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import io.github.lime3ds.android.overlay.*
+import android.content.Context
+import io.github.lime3ds.android.fragments.EmulationFragment
+//probably use emuation fragment because that's where the overlay is used
 
 
 class EditOverlayViewModel : ViewModel() {
